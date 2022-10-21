e76ff54c89b18a244b39da03b3ccbd4458cb87c1
Yasin Adam <74yadam@gmail.com>
2022-10-21 14:32:30 -0400

    just made profile page component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.css';
 import Home from './components/pages/Home';
 import Payments from './components/pages/Payments';
 import Messages from './components/pages/Messages';
+import Profile from './components/pages/Profile';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path='/accounts' element={<Accounts />} />
           <Route path='/payments' element={<Payments />} />
           <Route path='/messages' element={<Messages />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
