ded6492c4d787b9602e2191f5d7e5f941b7d3a76
Josiah S <josiah124@revature.net>
2022-11-01 22:53:20 -0400

    can register w/o restrictions

--- project2Data/src/main/java/com/revature/project2Data/service/AuthService.java
+++ project2Data/src/main/java/com/revature/project2Data/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.revature.project2Data.service;
 
+import com.revature.project2Data.model.User;
 import com.revature.project2Data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public User addUser(User newUser) {
+        return userRepository.save(newUser);
+    }
 }
